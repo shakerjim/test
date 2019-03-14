22c5518bb67e43e8bb9bcc0046e369824eb4f53d
zhenjie.zhang <zhenjie.zhang@ics8.com>
2019-03-14 16:00:22 +0800

    cc

--- src/lib/tool.js
+++
@@ -1,8 +0,0 @@
-var formatUrl = function (data) {
-  var _string = '';
-  Object.keys(data).forEach((item) => {
-    _string += encodeURI(item) + "=" + encodeURI(data[item]) + "&";
-  })
-  return _string.substr(0, _string.length - 1);
-};
-export default formatUrl
